bc18b1a2235a6a3d92a2335dbedb9be8a8ff2186
supriya-kotturu <supriyakotturu@pm.me>
2021-03-24 00:59:18 +0530

    add palindrome and removeDuplicate functions

--- anonyFuncAndIIFE.js
+++ anonyFuncAndIIFE.js
@@ -2,12 +2,9 @@
 // WHY - used as callbacks
 // DEF - A function with no name is called an anonymous function
 
-
-
 // 1. Print odd numbers in an array
 const ARRY = [35, 52, 563, 76, 557, 8, 3];
 console.log(ARRY.filter((e) => e % 2 != 0));
-
 
 // 2. Convert all the strings to title caps in a string array
 const str = "HELLO WORLD";
@@ -17,10 +14,8 @@ str.split(" ").forEach((word) => {
 });
 console.log(res.join(" "));
 
-
 // 3. Sum of all numbers in an array
 console.log(ARRY.reduce((s, e) => s + e, 0));
-
 
 // 4. Return all the prime numbers in an array
 const isPrime = (n) => {
@@ -40,7 +35,26 @@ console.log(
   }).filter((e) => typeof e != "undefined")
 );
 
-
 // 5. Return all the palindromes in an array
-const PALINDROME_ARRY = ['raceCar', 'madam', 'november']
+const PALINDROME_ARRY = ["raceCar", "madam", "november"];
+res = [];
 
+const isPalindrome = (word) => {
+  return word.toLowerCase() == word.toLowerCase().split("").
+  reverse().join("");
+};
+
+for (let word of PALINDROME_ARRY) {
+  isPalindrome(word) ? res.push(word) : null;
+}
+console.log(res);
+
+// 6. Return median of two sorted arrays of same size 
+
+
+// 7. Remove duplicates from an array 
+let arryWithDuplicates = [3,42,2,4,42,4545,555,4,4,2,2,42,42]
+console.log([...new Set(arryWithDuplicates)])
+
+
+// 8. Rotate an array by k times and return the rotated array.
